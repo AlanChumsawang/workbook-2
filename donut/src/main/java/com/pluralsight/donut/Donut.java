88b916c87f52009e0d40fc7730e62463a9ee77cb
AlanChumsawang <alan.chumsawang@outlook.com>
2024-10-07 09:18:32 -0400

    Added practice with OOP principles

---
+++ donut/src/main/java/com/pluralsight/donut/Donut.java
@@ -0,0 +1,12 @@
+package com.pluralsight.donut;
+
+public class Donut {
+    String description;
+    int calories;
+    double price;
+
+    public String toString()
+    {
+        return description + price + calories;
+    }
+}
